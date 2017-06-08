c37265a9a87b42e97c275deec831b9e42cce709f
hadikingsinema <hadibigdirector@gmail.com>
2017-06-08 12:55:51 +0430

    Create seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "set",
     "get",
     "broadcast",
-    "download_media",
+    sudo_users = {hadi_safarzad},--sudo users
     "invite",
     "all",
     "leave_ban",
